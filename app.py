dfca130e7a00ea274620274d803be9d2c8e8d581
Danya <67584569+DarkGolly@users.noreply.github.com>
2023-04-16 19:48:37 +0300

    little fix

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask, request, render_template
-app = Flask(__name__)
-
-
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/post-handler', methods=['POST'])
@@ -16,7 +13,3 @@ def handle_post_request():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
-
-
-
