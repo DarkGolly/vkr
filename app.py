a1a277c9f60ce2ba84803ca1bcf070be9a560dcc
Danya <67584569+DarkGolly@users.noreply.github.com>
2023-04-27 15:22:42 +0300

    test long msg

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
-from pyais import decode
+from pyais import decode, NMEAMessage
+
 app = Flask(__name__)
 
 @app.route('/post-handler', methods=['POST'])
@@ -11,7 +12,18 @@ def handle_post_request():
         print("--------------------")
         decoded = decode(data)
         as_dict = decoded.asdict()
-        print(as_dict)
+        #print(as_dict)
+
+        msg_2_part_0 = b'!AIVDM,2,1,9,A,538CQ>02A;h?D9QC800pu8@T>0P4l9E8L0000017Ah:;;5r50Ahm5;C0,0*0F'
+        msg_2_part_1 = b'!AIVDM,2,2,9,A,F@V@00000000000,2*3D'
+
+        msg = NMEAMessage.assemble_from_iterable(
+            messages=[
+                NMEAMessage(msg_2_part_0),
+                NMEAMessage(msg_2_part_1)
+            ]
+        ).decode().to_json()
+        print(msg)
         return f'The data you sent was: {data}'
     else:
         return 'Unsupported Media Type', 415
