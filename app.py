b85faac90c458286a912497ecaffb199c64b4d13
DarkGolly <geuwk65@gmail.com>
2023-04-29 17:31:17 +0300

    test6

--- app.py
+++ app.py
@@ -6,15 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    data = ["!AIVDM,2,1,2,A,53Kt>r02BF18ta0>220eDm1Dh622222222222216D8T:F79V0ATUH31A8888,0*51",
-            "!AIVDM,2,2,2,A,88888888880,2*26"]
-    res = NMEAMessage.assemble_from_iterable(
-        messages=[
-            NMEAMessage(str.encode(data[0])),
-            NMEAMessage(str.encode(data[1]))
-        ]
-    ).decode().to_json()
-    return res
+    return "This is VKR."
 @app.route('/post-handler', methods=['POST'])
 def handle_post_request():
     if request.content_type == 'text/plain':
@@ -27,10 +19,14 @@ def handle_post_request():
                     NMEAMessage(str.encode(splitData[1]))
                 ]
             ).decode().asdict()
+            print(splitData)
+            print("------")
             print(res)
         else:
             decoded = decode(splitData[0])
             as_dict = decoded.asdict()
+            print(splitData)
+            print("-------")
             print(as_dict)
         return f'The data you sent was: {data}'
     else:
