943b0d14181ef70fc7d2ee662989693884b0e86b
Danya <67584569+DarkGolly@users.noreply.github.com>
2023-04-30 03:34:50 +0300

    fix cursor db

--- db.py
+++ db.py
@@ -2,24 +2,24 @@ import sqlite3
 
 class DataBase:
     connection = None
+    cursor = None
     def __init__(self):
         try:
             self.connection = sqlite3.connect('navigacions_data.db')
+            self.cursor = self.connection.cursor()
             print("База данных успешно подключена к SQLite")
         except sqlite3.Error as error:
             print("Ошибка при подключении к sqlite", error)
 
     def execute_query(self):
-        cursor = self.connection.cursor()
         sqlite_select_query = "select sqlite_version();"
-        cursor.execute(sqlite_select_query)
-        record = cursor.fetchall()
+        self.cursor.execute(sqlite_select_query)
+        record = self.cursor.fetchall()
         print("Версия базы данных SQLite: ", record)
 
     def add_data(self, dict):
-        cursor = self.connection.cursor()
         columns = ', '.join(dict.keys())
         placeholders = ':' + ', :'.join(dict.keys())
         query = f'INSERT INTO {"ais_data"} ({columns}) VALUES ({placeholders})'
-        cursor.execute(query, dict)
+        self.cursor.execute(query, dict)
         self.connection.commit()
